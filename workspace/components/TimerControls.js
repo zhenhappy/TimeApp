ec81c3f3bdf5d940f6ef492a7d3fedb79f6d91e6
zhenhappy <q505507538@gmail.com>
2016-07-12 13:37:35 +0800

    ii

--- workspace/components/TimerControls.js
+++ workspace/components/TimerControls.js
@@ -9,7 +9,8 @@ export class TimerControls extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image source={require('../imgs/start-icon.png')} />
+        <Image source={require('./imgs/start-icon.png')} />  // 成功
+        <Image source={require('../imgs/start-icon.png')} /> // 失败
       </View>
     );
   }
